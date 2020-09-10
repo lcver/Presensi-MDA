3ecb49aeb0a645f15dc17d234f656203d9d8cc74
lcver <crackover1092@gmail.com>
2020-09-11 05:32:40 +0700

    [fix] URL Redirec

--- App/views/Template/peserta/navbar.php
+++ App/views/Template/peserta/navbar.php
@@ -1,7 +1,7 @@
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
 <div class="container">
-    <a href="" class="navbar-brand">
+    <a href="<?=BASEURL?>" class="navbar-brand">
     <img src="<?=BASEURL?>img/logoPPG.jpeg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8; width:50px;height:50px;">
     <span class="brand-text font-weight-light">Presensi Online</span>
